036e41647bb5e0a8b9f48721dbc8d2748c03fc9d
Michal Bil <michal.bil@fakro.pl>
2024-01-14 13:36:12 +0100

    zadanie numer 3

---
+++ test.js
@@ -0,0 +1,10 @@
+//Zadanie 1
+function show() {
+    console.log("hello world!");
+}
+show();
+//Zadanie 2
+function calc(a, b) {
+    return a * b;
+}
+console.log(calc(2, 5));
---
+++ test.ts
@@ -0,0 +1,16 @@
+//Zadanie 1
+
+function show() {
+  console.log("hello world!");
+}
+
+show();
+
+//Zadanie 2
+
+function calc(a: number, b: number): number {
+  return a * b;
+}
+
+console.log(calc(2, 5));
+
