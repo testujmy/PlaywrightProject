6eb55974ea4ee5e32123935da9f7f26337afd7ea
Michal Bil <michal.bil@fakro.pl>
2024-01-14 21:17:52 +0100

    Playwright lekcja 3

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,18 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, chromium } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('Poprawne zalogowanie do banku', async ({ page }) => {
+  //przejście pod adress url
+  await page.goto("https://demo-bank.vercel.app/");
+  //oczekiwanie na pełne załadowanie strony czyli wyświetlenie elementu
+  await page.waitForSelector("//input[@data-testid='login-input']");
+  //wpisanie loginu
+  await page.fill("//input[@data-testid='login-input']", "12345678");
+  //wpisanie hasła
+  await page.fill("//input[@data-testid='password-input']", "12345678");
+  //Kliknięcie przycisku zaloguj
+  await page.click("//button[@data-testid='login-button']");
+  //oczekiwanie na pojawienie elementu
+  await page.waitForSelector("//a[@data-testid='user-name']");
+  //assercja sprawdzenie czy pojawił sie element który jest dostępny tylko po zalogowaniu
+  expect(await page.locator("//a[@data-testid='user-name']").isVisible()).toBeTruthy();
 });
