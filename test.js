94ab27252af7031e1480b72be74c8ecfb6520650
Michal Bil <michal.bil@fakro.pl>
2024-01-14 12:32:30 +0100

    zadanie lekcja 1

---
+++ test.js
@@ -0,0 +1,3 @@
+//Zadanie 1
+var variableOne = 123;
+var variableTwo = variableOne.toString();
---
+++ test.ts
@@ -0,0 +1,4 @@
+//Zadanie 1
+let variableOne: number = 123;
+const variableTwo = variableOne.toString();
+
