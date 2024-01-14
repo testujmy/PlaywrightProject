46edb3290383578fdda9d6e1fc6b852599f7b1e7
Michal Bil <michal.bil@fakro.pl>
2024-01-14 13:28:06 +0100

    zadanie numer 2

---
+++ test.js
@@ -0,0 +1,18 @@
+//Zadanie 1
+var number = 5;
+if (number === 5) {
+    console.log("Warunek spełniony");
+}
+else {
+    console.log("Warunek nie spełniony");
+}
+//Zadanie 2
+for (var x = 0; x < number; x++) {
+    console.log("Petla for: " + x);
+}
+//Zadanie 3
+var a = 0;
+while (a < number) {
+    console.log("Pętla while: " + a);
+    a++;
+}
---
+++ test.ts
@@ -0,0 +1,19 @@
+//Zadanie 1
+let number = 5;
+if(number === 5) {
+  console.log("Warunek spełniony");
+} else {
+  console.log("Warunek nie spełniony");
+}
+
+//Zadanie 2
+for(let x = 0; x < number; x++) {
+  console.log("Petla for: " + x);
+}
+
+//Zadanie 3
+let a = 0;
+while(a < number) {
+  console.log("Pętla while: " + a);
+  a++;
+}
