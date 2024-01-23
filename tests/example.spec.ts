74b216b8cfa37bcc2c67dff198bdb1b351dfbb49
Michal Bil <michal.bil@fakro.pl>
2024-01-23 20:40:03 +0100

    lekcja numer 4

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,46 @@
-import { test, expect, chromium } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-test('Poprawne zalogowanie do banku', async ({ page }) => {
-  //przejście pod adress url
+test('Poprawne zalogowanie do banku przy uzyciu lokatorow wbudowanych', async ({ page }) => {
+  //przejscie pod adress url
   await page.goto("https://demo-bank.vercel.app/");
-  //oczekiwanie na pełne załadowanie strony czyli wyświetlenie elementu
-  await page.waitForSelector("//input[@data-testid='login-input']");
-  //wpisanie loginu
-  await page.fill("//input[@data-testid='login-input']", "12345678");
-  //wpisanie hasła
-  await page.fill("//input[@data-testid='password-input']", "12345678");
-  //Kliknięcie przycisku zaloguj
-  await page.click("//button[@data-testid='login-button']");
-  //oczekiwanie na pojawienie elementu
-  await page.waitForSelector("//a[@data-testid='user-name']");
-  //assercja sprawdzenie czy pojawił sie element który jest dostępny tylko po zalogowaniu
-  expect(await page.locator("//a[@data-testid='user-name']").isVisible()).toBeTruthy();
+  //wpisanie do pola identyfikator wartosci
+  await page.getByTestId("login-input").fill("12345678");
+  //wpisanie do pola haslo wartosci
+  await page.getByTestId("password-input").fill("87654321");
+  //klikniecie przycisku zaloguj sie
+  await page.getByRole("button").click();
+  //assercja sprawdzajaca czy przycisk wyloguj sie pojawil sie na stronie
+  //funkcja isVisible() zwraca true jesli element jest widoczny lub false
+  //jesli element nie jest widoczny
+  expect(await page.getByTestId("logout-button").isVisible()).toBeTruthy;
+});
+
+test('Poprawne zalogowanie do banku przy uzyciu lokatorow css', async ({ page }) => {
+  //przejscie pod adress url
+  await page.goto("https://demo-bank.vercel.app/");
+  //wpisanie do pola identyfikator wartosci
+  await page.locator("#login_id").fill("12345678");
+  //wpisanie do pola haslo wartosci
+  await page.locator("#login_password").fill("87654321");
+  //klikniecie przycisku zaloguj sie
+  await page.locator("#login-btn").click();
+  //assercja sprawdzajaca czy przycisk wyloguj sie pojawil sie na stronie
+  //funkcja isVisible() zwraca true jesli element jest widoczny lub false
+  //jesli element nie jest widoczny
+  expect(await page.locator("#log_out").isVisible()).toBeTruthy;
+});
+
+test('Poprawne zalogowanie do banku przy uzyciu lokatorow xpath', async ({ page }) => {
+  //przejscie pod adress url
+  await page.goto("https://demo-bank.vercel.app/");
+  //wpisanie do pola identyfikator wartosci
+  await page.locator("//input[@id='login_id']").fill("12345678");
+  //wpisanie do pola haslo wartosci
+  await page.locator("#login_password").fill("87654321");
+  //klikniecie przycisku zaloguj sie
+  await page.locator("//button[@id='login-btn']").click();
+  //assercja sprawdzajaca czy przycisk wyloguj sie pojawil sie na stronie
+  //funkcja isVisible() zwraca true jesli element jest widoczny lub false
+  //jesli element nie jest widoczny
+  expect(await page.locator("//a[@id='log_out']").isVisible()).toBeTruthy;
 });
