e6856cac6ec4310cdc38958b633bb209907023c6
Michal Bil <michal.bil@fakro.pl>
2024-01-25 19:57:55 +0100

    kod do lekcji 8

--- tests/transfer/test/transferTest.spec.ts
+++ tests/transfer/test/transferTest.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test';
 import { TransferAction } from '../../transfer/action/transferAction';
 import { LoginAction } from '../../login/action/loginAction';
+import { faker } from '@faker-js/faker';
 
 //stworzenie zmiennych ktore przechowuja nasze obiekty
 let loginAction: LoginAction;
@@ -15,12 +16,16 @@ test.beforeEach(async ({ page }) => {
 
 test("Wykonanie przelewu bankowego", async ({ }) => {
   //wszystkie dane jakie bedziemy potrzebowac w tescie
-  const login = "12345678k";
-  const password = "87654321k";
+  //faker losuje 8 liter
+  const login = faker.string.alpha({ length: 8 });
+  const password = faker.string.alpha({ length: 8 })
   const url = "https://demobank.jaktestowac.pl/logowanie_etap_2.html";
-  const recipent = "test";
-  const accountNumber = "11111111111111111111111111";
-  const price = "200";
+  //faker losuje imie
+  const recipent = faker.person.firstName();
+  //faker losuje numer konta o 26 znakach
+  const accountNumber = faker.finance.accountNumber(26);
+  //faker losuje cene z przdzialu 100 - 500
+  const price = faker.commerce.price({ min: 100, max: 500 });
 
   //uzycie akcji ktore zostaly napisane dla stron
   await loginAction.loginToBank(url, login, password);
